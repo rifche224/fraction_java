c840db17d7161461c2b5b5f06e12f567b1ceb79b
amandine-malo <amandine.malo@outlook.com>
2019-10-17 11:29:28 +0200

    question 1 et 2 faites

--- projet_fraction/src/main/java/tp_fraction/projet_fraction/Fraction.java
+++ projet_fraction/src/main/java/tp_fraction/projet_fraction/Fraction.java
@@ -4,7 +4,16 @@ public class  Fraction {
 	private int num;
 	private int denum;
 	
+	final public Fraction ZERO = new Fraction(0,1);
+	final public Fraction UN = new Fraction(1,1);
+	
+	public Fraction() {
+		this.num = 0;
+		this.denum = 1;
+	}
+	
 	public Fraction(int n, int d) {
+		if (d == 0) throw new IllegalArgumentException("Denominateur egal a 0");
 		this.num = n;
 		this.denum = d;
 	}
