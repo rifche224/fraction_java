ca15f917de066d6c5c06dd7c371f6213b49360ca
cherif <cherif.sy@hotmail.com>
2019-10-17 11:21:09 +0200

    ceci est mon deuxiemme commit

--- projet_fraction/src/main/java/tp_fraction/projet_fraction/Fraction.java
+++ projet_fraction/src/main/java/tp_fraction/projet_fraction/Fraction.java
@@ -8,4 +8,28 @@ public class  Fraction {
 		this.num = n;
 		this.denum = d;
 	}
+ 
+	public void fraction_initial(int n) {
+		 this.denum=1;
+		 this.num=n;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getDenum() {
+		return denum;
+	}
+
+	public void setDenum(int denum) {
+		this.denum = denum;
+	}
+
+	
+	
 }
